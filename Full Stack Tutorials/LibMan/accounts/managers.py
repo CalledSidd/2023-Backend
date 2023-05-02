ae1d490c5e689860d51fa1c5e49c645b5f2a3c57
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-05-03 00:55:56 +0530

    django-debug-toolbar

---
+++ Full Stack Tutorials/LibMan/accounts/managers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError(_("User Requires an Email"))
+            email = self.normalize_email(email)
+            user = self.model(email=email, **kwargs)
+            user.set_password(password)
+            user.save()
+            return user 
+        
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("is_active", True)
+
+        if kwargs.get("is_staff") or kwargs.get("is_superuser") is not True:
+            raise ValueError(_("Must be staff and superuser"))
+        return self.create_user(email, password, **kwargs)
--- Full Stack Tutorials/LibMan/accounts/models.py
+++ Full Stack Tutorials/LibMan/accounts/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from . managers import CustomUserManager
 
 # Create your models here.
+
+class Books(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    author = models.CharField(max_length=100)
+    sponsored_by = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Borrowed(models.Model):
+    user = models.OneToOneField(Member, on_delete=models.CASCADE)
+    book = models.OneToOneField(Books, on_delete=models.CASCADE)
+
+
+
+class Member(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email_address"), unique=True)
+    name  = models.CharField(max_length=100)
+    member_from = models.DateField(auto_now_add=True)
+    borrowed_books = models.ForeignKey(Borrowed, on_delete=models.CASCADE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
+    
