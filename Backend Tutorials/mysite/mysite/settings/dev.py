b7d60a356743313a8fc2ea7230091d1c752a2174
Sidharth S <64461639+CalledSidd@users.noreply.github.com>
2023-05-09 10:33:19 +0530

    wagtail-cms

---
+++ Backend Tutorials/mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-a2)@mcvz6m_#6)=h*-u)ajmh45erz=8%44l))wpn9aj(+-87e="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
