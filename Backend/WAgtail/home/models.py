77d9b9b182459b4b09311552a96fd4596a89c326
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-11 05:29:12 +0530

    doing-models for wagtail app

--- Backend/WAgtail/home/models.py
+++ Backend/WAgtail/home/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
     template = 'home/home_page.html'
-
+    blog_title = models.CharField(max_length=100, blank=True, null=True)
+    content_panels = Page.content_panels + [
+        FieldPanel("banner title"),
+    ]
