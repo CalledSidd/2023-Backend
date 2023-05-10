d7816a55d455fef441231d8cab12dbfd2fde0ec1
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-10 13:37:27 +0530

    Deleted All: Redo

--- Backend Tutorials/basics/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from blog.models import Post
-
-# Register your models here.
-
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug','author')
-admin.site.register(Post, AuthorAdmin)
