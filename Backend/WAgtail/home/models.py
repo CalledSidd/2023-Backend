de8fa91b9957ca7ddff22c61edaaf14ec6d68cdd
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-11 05:33:36 +0530

    first title is visible now

--- Backend/WAgtail/home/models.py
+++ Backend/WAgtail/home/models.py
@@ -8,5 +8,5 @@ class HomePage(Page):
     template = 'home/home_page.html'
     blog_title = models.CharField(max_length=100, blank=True, null=True)
     content_panels = Page.content_panels + [
-        FieldPanel("banner title"),
+        FieldPanel("blog_title"),
     ]
