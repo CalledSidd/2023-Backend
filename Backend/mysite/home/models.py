c9bb2bc22feaab40f450ce142e3d15a9cf0f7d82
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-11 11:37:41 +0530

    still on the models : added cta and image field

--- Backend/mysite/home/models.py
+++ Backend/mysite/home/models.py
@@ -2,10 +2,29 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 
 class HomePage(Page):
     templates = 'home/home_page.html'
     blog_title = models.CharField(max_length=100, blank=False, null=True)
+    blog_subttle = RichTextField(features = ["bold", "italic"])
+    blog_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    blog_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+
+    )
+
+
     content_panels = Page.content_panels + [
         FieldPanel('blog_title')
     ]
