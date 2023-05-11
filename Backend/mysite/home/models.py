de46ed7b69fcddf35881bf98d39b14ae890989a7
CalledSidd <sidharthsidhu12317@gmail.com>
2023-05-11 05:58:14 +0530

    Working but welll... well not broke won't fix it

--- Backend/mysite/home/models.py
+++ Backend/mysite/home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
     templates = 'home/home_page.html'
+    blog_title = models.CharField(max_length=100, blank=False, null=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('blog_title')
+    ]
+
+    class Meta:
+        verbose_name = "OH HEY THERE"
+        verbose_name_plural = "PLURAL THERE"
